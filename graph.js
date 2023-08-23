e70593ab1a43f45508db41bf3f60b849e49d9a5c
Yangyang Li <yangyang.li@northwestern.edu>
2023-08-23 17:22:01 -0500

    first workable version

---
+++ graph.js
@@ -0,0 +1,131 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let cy;
+    let walks = [];
+
+    // Fetching the data from the JSON file
+    fetch("graphData.json")
+        .then((response) => response.json())
+        .then((graphData) => {
+            cy = cytoscape({
+                container: document.getElementById("cy"),
+                elements: graphData,
+                layout: {
+                    name: "dagre",
+                    fit: true,
+                    padding: 10,
+                    avoidOverlap: true,
+                    rankDir: "LR",
+                },
+                style: [
+                    {
+                        selector: "node",
+                        style: {
+                            label: "data(name)",
+                            "background-color": "#666",
+                        },
+                    },
+                    {
+                        selector: "node[source-node]",
+                        style: {
+                            "background-color": "#FF5733",
+                        },
+                    },
+                    {
+                        selector: "edge",
+                        style: {
+                            width: "data(weight)",
+                            label: "data(label)",
+                            "text-rotation": "autorotate",
+                            "curve-style": "bezier",
+                            "target-arrow-shape": "triangle",
+                        },
+                    },
+                ],
+
+                // initial viewport state:
+                zoom: 1,
+                pan: { x: 0, y: 0 },
+
+                // interaction options:
+                minZoom: 0.1,
+                maxZoom: 3,
+                zoomingEnabled: true,
+                userZoomingEnabled: true,
+                panningEnabled: true,
+                userPanningEnabled: true,
+                boxSelectionEnabled: false,
+                selectionType: "single",
+                touchTapThreshold: 8,
+                desktopTapThreshold: 4,
+                autolock: false,
+                autoungrabify: false,
+                elements: graphData,
+            });
+
+            cy.nodes().forEach((node) => {
+                if (
+                    node.outgoers().edges().length > 0 &&
+                    node.incomers().edges().length === 0
+                ) {
+                    node.data("source-node", true);
+                }
+            });
+
+            let sourceNodes = cy.nodes().filter((node) => node.indegree() === 0);
+            let sinkNodes = cy.nodes().filter((node) => node.outdegree() === 0);
+
+            sourceNodes.forEach((sourceNode) => {
+                dfs(sourceNode, [], sinkNodes);
+            });
+
+            displayWalks();
+        })
+        .catch((error) => console.error("Failed to fetch graph data:", error));
+
+    function dfs(node, currentPath, sinkNodes) {
+        currentPath.push(node);
+
+        if (sinkNodes.includes(node)) {
+            walks.push([...currentPath]); // Found a path
+        } else {
+            let neighbors = node.outgoers().nodes();
+            neighbors.forEach((neighbor) => {
+                dfs(neighbor, currentPath, sinkNodes);
+            });
+        }
+
+        currentPath.pop(); // backtrack
+    }
+
+    function displayWalks() {
+        let walksContainer = document.getElementById("walks");
+        walks.forEach((walk, index) => {
+            let walkDiv = document.createElement("div");
+            walkDiv.textContent = `Walk ${index + 1}: ${walk
+                .map((node) => node.id())
+                .join(" -> ")}`;
+            walksContainer.appendChild(walkDiv);
+        });
+    }
+
+    function setupClickEvent() {
+        cy.on("tap", "node, edge", function(evt) {
+            let element = evt.target;
+
+            let info = "";
+            if (element.isNode()) {
+                info = `Node Information:\n\nID: ${element.id()}\nData: ${JSON.stringify(
+                    element.data(),
+                )}`;
+            } else if (element.isEdge()) {
+                info = `Edge Information:\n\nSource: ${element
+                    .source()
+                    .id()}\nTarget: ${element.target().id()}\nData: ${JSON.stringify(
+                        element.data(),
+                    )}`;
+            }
+
+            alert(info);
+        });
+    }
+});
