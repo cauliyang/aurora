bbe7cfd4be4fed456f3f901afa6957cc943eead4
Yangyang Li <yangyang.li@northwestern.edu>
2023-08-25 00:34:58 -0500

    add maximize button and feature

--- static/js/graph.js
+++ static/js/graph.js
@@ -386,4 +386,33 @@ function setupClickEvent(cy) {
     });
 }
 
+// Get references to the cy, info, and walks elements
+const cyContainer = document.getElementById("cy");
+const infoPanel = document.getElementById("info");
+const walksPanel = document.getElementById("walks");
+
+// Get references to the maximize button and set initial state
+const maximizeButton = document.getElementById("toggleMaximize");
+let isMaximized = false;
+
+// Add click event listener to the maximize button
+maximizeButton.addEventListener("click", function() {
+    if (isMaximized) {
+        // Restore previous layout
+        cyContainer.style.width = "";
+        cyContainer.style.height = "";
+        infoPanel.style.display = "";
+        walksPanel.style.display = "";
+        isMaximized = false;
+    } else {
+        // Maximize cy panel
+        cyContainer.style.width = "100%";
+        cyContainer.style.height = "100vh";
+        infoPanel.style.display = "none";
+        walksPanel.style.display = "none";
+        isMaximized = true;
+    }
+});
+
+
 export { loadGraphDataFromServer };
