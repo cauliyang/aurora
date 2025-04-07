8e517a1a56479d150da2b801f1bc6c57e20d0cc3
Yangyang Li <yangyang.li@northwestern.edu>
2025-04-07 13:57:32 -0500

    add tsg-core

---
+++ src/scripts/tsg.js
@@ -0,0 +1,10 @@
+import * as wasm from "./pkg/tsg_core_js.js";
+
+async function run() {
+  const wasmModule = await wasm.default();
+  console.log("WASM module loaded");
+  let result = wasmModule.add(1, 2);
+  console.log(result);
+}
+
+run();
